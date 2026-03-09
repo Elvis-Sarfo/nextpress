c881d8f429d7edca1a3ab5282812d410f2cbfbd4
Elvis Sarfo Antwi <sarfoantwielvis@gmail.com>
2026-03-09 01:00:43 +0000

    ......

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,45 +1,6 @@
-import { aboutManifest } from './about';
-import { bannerManifest } from './banner';
-import { contactManifest } from './contact';
-import { faqManifest } from './faq';
-import { featuredRoomsManifest } from './featured-rooms';
-import { galleryManifest } from './gallery';
-import { heroManifest } from './hero';
-import { postListManifest } from './post-list';
-import { serviceManifest } from './service';
-import { showcaseManifest } from './showcase';
-import { subscribeManifest } from './subscribe';
-import { testimonialManifest } from './testimonial';
+import type { BlockManifest } from './types';
 import { toBlockTypeOption } from './define';
 
-export const blocks = [
-  heroManifest,
-  bannerManifest,
-  aboutManifest,
-  faqManifest,
-  galleryManifest,
-  serviceManifest,
-  testimonialManifest,
-  contactManifest,
-  subscribeManifest,
-  featuredRoomsManifest,
-  showcaseManifest,
-  postListManifest,
-];
+export const blocks: BlockManifest[] = [];
 
 export const blockTypeOptions = blocks.map(toBlockTypeOption);
-
-export {
-  aboutManifest,
-  bannerManifest,
-  contactManifest,
-  faqManifest,
-  featuredRoomsManifest,
-  galleryManifest,
-  heroManifest,
-  postListManifest,
-  serviceManifest,
-  showcaseManifest,
-  subscribeManifest,
-  testimonialManifest,
-};
